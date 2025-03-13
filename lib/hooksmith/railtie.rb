88369b59711d1590b371cf88b16b17568e5022c1
gregoryrivage <gregory@rivage.immo>
2025-03-13 17:17:41 +0100

    Remove logger

--- lib/hooksmith/railtie.rb
+++ lib/hooksmith/railtie.rb
@@ -7,8 +7,6 @@ if defined?(Rails)
     # This file allows Hooksmith to integrate seamlessly with a Rails application.
     class Railtie < Rails::Railtie
       initializer 'hooksmith.configure_rails_initialization' do |_|
-        Hooksmith.logger.info('Hooksmith initialized in Rails')
-        # Optionally set the gem logger to use Rails.logger if available.
         if defined?(Rails.logger) && Rails.logger
           Hooksmith::Logger.instance.instance_variable_set(:@logger, Rails.logger)
         end
